e05ab118391e24e042ce46b68084cd16312156d7
tuanmiles <maianhtuan3765@gmail.com>
2023-02-04 09:52:35 +0700

    test

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import Home from '../pages/Home';
+import Following from '../pages/Following'
+
+
+const publicRoutes = [
+    { path: '/', component: Home},
+    { path: '/following', component: Following},
+]
+
+const privateRoutes = [
+
+]
+
+export default {publicRoutes, privateRoutes}
